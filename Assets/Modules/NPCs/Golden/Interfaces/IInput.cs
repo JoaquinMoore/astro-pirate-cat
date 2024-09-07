7f9344ccff7062df78982d33586772d25895ca11
int0x80-sys <matt.sosa314@gmail.com>
2024-09-07 15:40:39 -0300

    Created NPC Golden Input Interface

---
+++ Assets/Modules/NPCs/Golden/Interfaces/IInput.cs
@@ -0,0 +1,9 @@
+namespace Assets.Modules.NPCs.Golden.Interfaces
+{
+    public interface IInput
+    {
+        public float HorizontalDirection();
+        public float VerticalDirection();
+        public bool Attack();
+    }
+}
