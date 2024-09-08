d3ea1ce1747d529789ce743fc20ad0de0d57b2ec
int0x80-sys <matt.sosa314@gmail.com>
2024-09-08 15:41:10 -0300

    Updated the namespace of the MovementTest class

--- Assets/Modules/NPCs/Golden/Tests/PlayMode/Movements/MovementTest.cs
+++ Assets/Modules/NPCs/Golden/Tests/PlayMode/Movements/MovementTest.cs
@@ -6,60 +6,63 @@ using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 using Assets.Modules.NPCs.Golden.Movements;
 
-public class MovementTest
+namespace Assets.Modules.NPCs.Golden.Tests.PlayMode.Movements
 {
-    private MovementContext _goldenMovementContext;
-    private MockInput _mockInput;
-
-    private struct Case
+    public class MovementTest
     {
-        public Vector2 direction;
-        public Vector2 expectedPosition;
-        public float seconds;
-    }
+        private MovementContext _goldenMovementContext;
+        private MockInput _mockInput;
 
-    [UnitySetUp]
-    public IEnumerator SetUp()
-    {
-        EditorSceneManager.LoadSceneInPlayMode(
-            "Assets/Modules/NPCs/Golden/Tests/PlayMode/Rebuilds/MovementBasic.unity",
-            new LoadSceneParameters(LoadSceneMode.Single)
-        );
-        yield return null;
-        _goldenMovementContext = Object.FindObjectOfType<MovementContext>();
-        _mockInput = _goldenMovementContext.GetComponent<MockInput>();
-    }
-
-    [UnityTest]
-    public IEnumerator TestIdleToWalkStateChange()
-    {
-        var seconds = 1;
-        var directions = new Vector2[]
+        private struct Case
         {
-            new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1),
-            new Vector2(-1, 1)
-        };
-        foreach (var direction in directions)
-        {
-            yield return Move(direction, seconds);
-            var finalXPosition = direction.normalized.x * _goldenMovementContext.Speed * seconds;
-            var finalYPosition = direction.normalized.y * _goldenMovementContext.Speed * seconds;
-            Assert.AreApproximatelyEqual(_goldenMovementContext.transform.position.x, finalXPosition, 0.1f);
-            Assert.AreApproximatelyEqual(_goldenMovementContext.transform.position.y, finalYPosition, 0.1f);
-            yield return Move(-direction, seconds);
-            Assert.AreApproximatelyEqual(_goldenMovementContext.transform.position.x, 0, 0.1f);
-            Assert.AreApproximatelyEqual(_goldenMovementContext.transform.position.y, 0, 0.1f);
+            public Vector2 direction;
+            public Vector2 expectedPosition;
+            public float seconds;
         }
-    }
 
-    private IEnumerator Move(Vector2 direction, float seconds)
-    {
-        _mockInput.HDirection = direction.x;
-        _mockInput.VDirection = direction.y;
-        yield return new WaitForSeconds(seconds);
-        _mockInput.HDirection = 0;
-        _mockInput.VDirection = 0;
+        [UnitySetUp]
+        public IEnumerator SetUp()
+        {
+            EditorSceneManager.LoadSceneInPlayMode(
+                "Assets/Modules/NPCs/Golden/Tests/PlayMode/Rebuilds/MovementBasic.unity",
+                new LoadSceneParameters(LoadSceneMode.Single)
+            );
+            yield return null;
+            _goldenMovementContext = Object.FindObjectOfType<MovementContext>();
+            _mockInput = _goldenMovementContext.GetComponent<MockInput>();
+        }
+
+        [UnityTest]
+        public IEnumerator TestIdleToWalkStateChange()
+        {
+            var seconds = 1;
+            var directions = new Vector2[]
+            {
+                new Vector2(1, 0),
+                new Vector2(1, 1),
+                new Vector2(0, 1),
+                new Vector2(-1, 1)
+            };
+            foreach (var direction in directions)
+            {
+                yield return Move(direction, seconds);
+                var finalXPosition = direction.normalized.x * _goldenMovementContext.Speed * seconds;
+                var finalYPosition = direction.normalized.y * _goldenMovementContext.Speed * seconds;
+                Assert.AreApproximatelyEqual(_goldenMovementContext.transform.position.x, finalXPosition, 0.1f);
+                Assert.AreApproximatelyEqual(_goldenMovementContext.transform.position.y, finalYPosition, 0.1f);
+                yield return Move(-direction, seconds);
+                Assert.AreApproximatelyEqual(_goldenMovementContext.transform.position.x, 0, 0.1f);
+                Assert.AreApproximatelyEqual(_goldenMovementContext.transform.position.y, 0, 0.1f);
+            }
+        }
+
+        private IEnumerator Move(Vector2 direction, float seconds)
+        {
+            _mockInput.HDirection = direction.x;
+            _mockInput.VDirection = direction.y;
+            yield return new WaitForSeconds(seconds);
+            _mockInput.HDirection = 0;
+            _mockInput.VDirection = 0;
+        }
     }
 }
