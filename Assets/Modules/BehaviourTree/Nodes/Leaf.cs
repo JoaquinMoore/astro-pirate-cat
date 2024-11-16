f124c39f32b193f5e31f06f1a31ef3b0563dc749
Joaquin <58609334+JoaquinMoore@users.noreply.github.com>
2024-11-16 20:30:36 -0300

    Modulos
    - Service locator
    - Behaviour tree
    - Extensions

---
+++ Assets/Modules/BehaviourTree/Nodes/Leaf.cs
@@ -0,0 +1,13 @@
+﻿public class Leaf : Node
+{
+    private readonly IStrategy _strategy;
+
+    public Leaf(string name, IStrategy strategy): base(name)
+    {
+        _strategy = strategy;
+    }
+
+    public override Status Process() => _strategy.Process();
+
+    public override void Reset() => _strategy.Reset();
+}
