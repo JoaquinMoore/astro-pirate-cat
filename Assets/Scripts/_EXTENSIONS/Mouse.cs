8d245efc70998a77bb29e2d9e012b9faa30def7c
Joaquin <58609334+JoaquinMoore@users.noreply.github.com>
2025-05-31 16:03:45 -0300

    Mejorando la extensión del MouseWorldPosition

--- Assets/Scripts/_EXTENSIONS/Mouse.cs
+++ Assets/Scripts/_EXTENSIONS/Mouse.cs
@@ -5,9 +5,13 @@ namespace _EXTENSIONS
 {
     public static class MouseExtensions
     {
+        private static Camera s_mainCamera;
+
+        private static Camera MainCamera => s_mainCamera ??= Camera.main;
+
         public static Vector2 WorldPosition(this Mouse mouse)
         {
-            return Camera.main.ScreenToWorldPoint(mouse.position.value);
+            return MainCamera.ScreenToWorldPoint(mouse.position.value);
         }
     }
 }
